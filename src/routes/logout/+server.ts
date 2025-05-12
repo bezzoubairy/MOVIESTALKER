32d9dd6066a2e7994f50b06ef1fb60da159e8faa
bezzoubairy <115186211+bezzoubairy@users.noreply.github.com>
2025-05-12 23:32:18 +0200

    Update +server.ts

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -10,8 +10,7 @@ export const POST: RequestHandler = async ({ cookies }) => {
   throw redirect(303, '/');
 };
 
-// Optionally, handle GET request if you want a simple link for logout
-// However, POST is generally preferred for actions that change state (like logging out)
+
 export const GET: RequestHandler = async ({ cookies }) => {
   // Clear the session cookie
   cookies.delete('session_id', { path: '/' });
