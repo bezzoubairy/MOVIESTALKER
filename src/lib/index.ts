42d9226e08e8ded855933923b0d15c0475fcb59e
bezzoubairy <115186211+bezzoubairy@users.noreply.github.com>
2025-05-28 01:03:32 +0200

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,28 @@
-// Reexport your entry components here
+declare global {
+	namespace App {
+		interface Error {
+			message: string;
+			status?: number;
+		}
+		
+		interface Locals {
+			user: {
+				id: string;
+				username: string;
+				email: string;
+			} | null;
+		}
+		
+		interface PageData {
+			currentUser?: {
+				id: string;
+				username: string;
+				email: string;
+			} | null;
+			error?: string;
+		}
+		
+	}
+}
+
+export {};
