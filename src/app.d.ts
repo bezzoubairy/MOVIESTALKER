4edf4e9c7c888148a6559dc64e04a9176229473f
bezzoubairy <115186211+bezzoubairy@users.noreply.github.com>
2025-05-28 01:04:14 +0200

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,28 @@
+
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Error {
+			message: string;
+			status?: number;
+		}
+		
+		interface Locals {
+			user: {
+				id: string;
+				username: string;
+				email: string;
+			} | null;
+		}
+		
+		interface PageData {
+			currentUser?: {
+				id: string;
+				username: string;
+				email: string;
+			} | null;
+			error?: string;
+		}
+		
 	}
 }
 
